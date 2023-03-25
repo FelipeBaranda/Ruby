4b174ed879d89d78ab12b8e9be346f0513ccea61
Luis Felipe Baranda <felbaranda@gmail.com>
2023-03-25 18:49:52 -0300

    estudo1

---
+++ lib/my_study_plan_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "my_study_plan_ruby/version"
+
+module MyStudyPlanRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/my_study_plan_ruby_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.describe MyStudyPlanRuby do
+  it "has a version number" do
+    expect(MyStudyPlanRuby::VERSION).not_to be nil
+  end
+
+
+end
